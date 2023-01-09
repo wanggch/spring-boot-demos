ac5883a2155c29242a57639c54584afc4600dc6b
wanggc <wang.guocheng@hotmail.com>
2023-01-09 17:54:48 +0800

    first comiit

---
+++ 01.spring-boot-hello/src/main/java/cn/ddcherry/springboot/demo/App.java
@@ -0,0 +1,19 @@
+package cn.ddcherry.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(App.class);
+        System.out.println("Beans: " + context.getBeanDefinitionCount());
+        String[] beanNames = context.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        Arrays.asList(beanNames).forEach(System.out::println);
+    }
+}
