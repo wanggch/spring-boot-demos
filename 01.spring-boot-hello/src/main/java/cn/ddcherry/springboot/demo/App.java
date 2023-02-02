ada6bf72528069d50aacd40045f7743789645c26
wanggc <wang.guocheng@hotmail.com>
2023-02-02 18:08:25 +0800

    20230202

--- 01.spring-boot-hello/src/main/java/cn/ddcherry/springboot/demo/App.java
+++ 01.spring-boot-hello/src/main/java/cn/ddcherry/springboot/demo/App.java
@@ -1,18 +1,26 @@
 package cn.ddcherry.springboot.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import java.net.InetAddress;
 import java.util.Arrays;
 
+@Slf4j
 @SpringBootApplication
 public class App {
-    public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(App.class);
-        System.out.println("Beans: " + context.getBeanDefinitionCount());
-        String[] beanNames = context.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        Arrays.asList(beanNames).forEach(System.out::println);
-    }
+	public static void main(String[] args) throws Exception {
+		// 获取本机IP地址
+		String ip = InetAddress.getLocalHost().getHostAddress();
+		log.info("本机ip: {}", ip);
+
+		ApplicationContext context = SpringApplication.run(App.class);
+		System.out.println("Bean个数: " + context.getBeanDefinitionCount());
+		String[] beanNames = context.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		System.out.println("########## Bean列表 ##########");
+		Arrays.asList(beanNames).forEach(System.out::println);
+	}
 }
